b17001930a1f63a588ce665955b6b8efb35c9b75
bailleulbe <bailleulbe@FR-D135741.groupinfra.com>
2015-10-26 13:23:54 +0100

    creation des sous-dossier et premiere fabrique(Patient)

--- Hopital/src/domaine/FicheSejour.java
+++ Hopital/src/domaine/FicheSejour.java
@@ -1,3 +1,4 @@
+package domaine;
 import java.util.ArrayList;
 import java.util.HashMap;
 
---
+++ Hopital/src/fabrique/FabriquePatient.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package fabrique;
+
+/**
+ * @author Léa Vanelle, Benoît Bailleul
+ *
+ */
+public class FabriquePatient {
+
+	static private FabriquePatient INSTANCE=null;
+	
+	
+	
+	
+	
+}
