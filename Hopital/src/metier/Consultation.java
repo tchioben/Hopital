b7654928364d3c4ae91fcc49151fcfacbcf802be
bailleulbe <bailleulbe@FR-D135741.groupinfra.com>
2015-10-27 14:25:18 +0100

    commit consultation

--- Hopital/src/metier/Consultation.java
+++ Hopital/src/metier/Consultation.java
@@ -3,6 +3,12 @@
  */
 package metier;
 
+import domaine.Patient;
+import domaine.Specialiste;
+import fabrique.FabriqueCompteRendu;
+import fabrique.FabriquePatient;
+import fabrique.FabriqueSpecialiste;
+
 /** Consultation
  * 
  * acteur : le spécialiste
@@ -14,5 +20,32 @@ package metier;
  * @author Léa Vanelle, Benoît Bailleul
  */
 public class Consultation {
-
+	/**
+	 * ajoute une consultation
+	 * associe le medecin au patient
+	 * @param nomMedecin
+	 * @param nomPatient
+	 */
+	public void ajouteConsultation(String nomMedecin, String nomPatient){
+		FabriqueSpecialiste fs = FabriqueSpecialiste.getINSTANCE();
+		Specialiste specialiste= fs.searchSpecialiste(nomMedecin);
+		FabriquePatient fp = FabriquePatient.getINSTANCE();
+		Patient patient = fp.searchPatient(nomPatient);
+		if (specialiste== null){
+			System.out.println("Le medecin n'existe pas");
+		}
+		else {
+			if(patient==null){
+				System.out.println("Le Patient n'existe pas");
+			}
+			else {
+				FabriqueCompteRendu fcr = FabriqueCompteRendu.getINSTANCE();
+				/**
+				 * faire la liste des comptes rendu, on regarge les medecin qui ont une cette specialite et pour chaque medecin on recuperere sa liste de compte rendu)
+				 * 
+				 */
+			}
+		}
+	}
+		
 }
